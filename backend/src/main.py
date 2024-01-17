6ab3605fcdcc55e5c4fc6e8f15d4badec392c463
Blackgolyb <andrejomelnickij@gmail.com>
2024-01-17 15:19:59 +0200

    added autoreload

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,15 +3,28 @@ from fastapi import FastAPI
 from utils.logger import init_logger
 
 from src.api.v1.api import api_router
+from src.core.config import get_settings
 
 
-def main() -> None:
-    app = FastAPI(title="Test Uvicorn Handlers")
+def configure_app() -> FastAPI:
+    setting = get_settings()
+
+    app = FastAPI(title="Test Uvicorn Handlers", debug=setting.core.debug)
     app.include_router(api_router)
 
     init_logger(app)
 
-    uvicorn.run(app, port=8009)
+    return app
+
+
+def main() -> None:
+    setting = get_settings()
+    uvicorn.run(
+        "main:configure_app",
+        port=8009,
+        reload=setting.core.debug,
+        factory=True,
+    )
 
 
 if __name__ == "__main__":
